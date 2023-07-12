d9a7fc73da59a6bc7bf105831c51b0542426ff05
Triana Bares <68872407+trianabares@users.noreply.github.com>
2023-07-12 02:43:10 -0300

    PrimerPrograma Java

---
+++ PrimerPrograma/PrimerPrograma.java
@@ -0,0 +1,7 @@
+public class PrimerPrograma {
+    public static void main(String[] args){
+        System.out.println("Vivo en San José de Mayo");
+        System.out.println("Tengo 24 años de edad");
+        System.out.println("Vivo en San José de Mayo");
+    }
+}
