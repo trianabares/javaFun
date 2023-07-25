e5c7c9448c9a2f565430dd31c6f90d5aff40f4bd
Triana Bares <68872407+trianabares@users.noreply.github.com>
2023-07-25 02:45:55 -0300

    HashMap

--- HashMap/HashMapFun.java
+++ HashMap/HashMapFun.java
@@ -4,15 +4,22 @@ import java.util.Set;
 public class HashMapFun {
     public static void main(String[] args) {
         HashMap<String, String> trackList = new HashMap<>();
-        trackList.put("Canción 1", "Letra de la canción 1");
-        trackList.put("Canción 2", "Letra de la canción 2");
-        trackList.put("Canción 3", "Letra de la canción 3");
-        trackList.put("Canción 4", "Letra de la canción 4");
+        trackList.put("I'm Still Standing", "You could never know what it's like");
+        trackList.put("Black Sheep", "Hello again, friend of a friend, I knew you when");
+        trackList.put("Pretty Old Man", "Your breath blows my hair in this crowded space");
+        trackList.put("Hold On Tight", "Baby, you and me are a twisted fantasy");
 
+        // Obtene una canción por su título
+        String title = "Canción 2";
+        System.out.println("Canción: " + title);
+
+        // Imprime todos los nombres de las pistas y las letras
+        System.out.println("----- Lista de canciones -----");
         Set<String> keys = trackList.keySet();
-        for(String key : keys) {
+        for (String key : keys) {
             System.out.println(key);
-            System.out.println(trackList.get(key));    
+            System.out.println(trackList.get(key));
         }
+
     }
 }
